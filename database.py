fb54b3056b13c9fdd96365ac967664a711a1e362
basit <ab4382906@gmail.com>
2025-09-08 17:45:46 +0500

    Remove Api keys from main code base

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-# example: "postgresql://postgres:your_password@localhost:5432/FastJwt"
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/FastJwt"
-
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
 
