71278c3f6b058bd4c048f239735cf990ee3d0798
basit <ab4382906@gmail.com>
2025-09-10 16:11:23 +0500

    Fix merge conflicts and deployment settings

--- main.py
+++ main.py
@@ -130,7 +130,3 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Default to 8000 locally
     uvicorn.run("main:app", host="0.0.0.0", port=port)
-<<<<<<< HEAD
-=======
-
->>>>>>> 8858e4125c6797ee09217e14307d9e6b42a900ff
